3168f7487e51c44b83323e679de7f94256739965
Anita Nersesyan <104271200+Anitanersesyan@users.noreply.github.com>
2025-06-16 20:56:07 +0200

    Update SocialMediaItem.js

--- components/ui/SocialMediaItem.js
+++ components/ui/SocialMediaItem.js
@@ -1,9 +1,3 @@
-/* TASK - React 1 week 2 */
-/* Create a <SocialMediaItem /> component and replace all of the list items! */
-/* it should accept the following props */
-/* url, title, icon */
-/* For the icons, you can download 1-2 social media icons for testing and put it in the /public/socialmedia/ folder */
-
 "use client";
 
 import Link from "next/link";
